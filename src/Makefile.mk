7bd6ef1f4d78c8d2ab73b18c94fbe058df403e1f
Bart Bilos <squantmuts@gmail.com>
2019-03-15 16:35:51 +0100

    added dswritechar test frame

--- src/Makefile.mk
+++ src/Makefile.mk
@@ -2,7 +2,8 @@
 BIN_NAME = libsqlibembeddedc
 C_SOURCES = src/ansi/parse_ansi.c \
 src/queue/queue_string.c \
-src/cmdline/cmdline_prompt.c
+src/cmdline/cmdline_prompt.c \
+src/datastream/dswritechar.c src/datastream/dsreadchar.c
 INCLUDES = -Iinc -Iinc_internal
 COMPILE_C_FLAGS += -std=gnu11 -Wall -Wextra -Wno-main -fno-common -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections 
 COMPILE_CXX_FLAGS += -std=c++17 -Wall -Wextra -Wno-main -fno-common -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -fno-rtti -fno-exceptions 
