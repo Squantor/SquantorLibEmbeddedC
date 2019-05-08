482fcbd6952e6552d30d477f4e2c333c67a44c29
Bart Bilos <squantmuts@gmail.com>
2019-05-08 19:52:46 +0200

    starting parser library

--- src/Makefile.mk
+++ src/Makefile.mk
@@ -11,7 +11,8 @@ src/print/print_digit.c \
 src/print/print_hex_u8.c src/print/print_hex_u16.c src/print/print_hex_u32.c \
 src/print/print_dec_u16.c src/print/print_decnz_u16.c \
 src/print/print_dec_u32.c \
-src/print/print_bin_u32.c
+src/print/print_bin_u32.c \
+src/parse/parse_decimal.c src/parse/parse_hex.c
 INCLUDES = -Iinc -Iinc_internal
 COMPILE_C_FLAGS += -std=gnu11 -Wall -Wextra -Werror -Wno-main -fno-common -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections 
 COMPILE_CXX_FLAGS += -std=c++17 -Wall -Wextra -Werror -Wno-main -fno-common -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -fno-rtti -fno-exceptions 
