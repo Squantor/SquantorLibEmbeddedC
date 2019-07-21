22d89f4a75c0b9ff1a1de17c208f021b1093fee8
Bart Bilos <squantmuts@gmail.com>
2019-07-21 13:21:50 +0200

    added custom libc include path for bare pc platform

--- platforms/bare_PC.mk
+++ platforms/bare_PC.mk
@@ -3,6 +3,13 @@ TOOLCHAIN_PREFIX =
 C_SOURCES += 
 CXX_SOURCES +=
 S_SOURCES +=
+
+# the bare pc target needs its own libc include path to prevent build system includes of leaking in
+ifdef LIBC_INC
+INCLUDES += -I$(LIBC_INC)
+else
+$(error for the bare_PC the libc include path needs to be defined LIBC_INC)
+endif
 COMPILE_C_FLAGS += -nostdlib -fomit-frame-pointer -fno-stack-protector -fno-unwind-tables -fno-asynchronous-unwind-tables -fno-unroll-loops -fmerge-all-constants -fno-ident -mfpmath=sse -mfancy-math-387 -ffunction-sections -fdata-sections 
 COMPILE_CXX_FLAGS +=
 COMPILE_ASM_FLAGS += -c -x assembler-with-cpp
