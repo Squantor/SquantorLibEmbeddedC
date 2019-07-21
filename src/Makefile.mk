5d379ba3542c1ae2eed29edf207ea7bf9bcdc923
Bart Bilos <squantmuts@gmail.com>
2019-07-21 13:27:18 +0200

    added custom libc include dir to libembeddedc bare pc build

--- src/Makefile.mk
+++ src/Makefile.mk
@@ -12,7 +12,7 @@ src/print/print_hex_u8.c src/print/print_hex_u16.c src/print/print_hex_u32.c \
 src/print/print_dec_u16.c src/print/print_decnz_u16.c \
 src/print/print_dec_u32.c \
 src/print/print_bin_u32.c
-INCLUDES = -Iinc -Iinc_internal
+INCLUDES += -Iinc -Iinc_internal
 COMPILE_C_FLAGS += -std=gnu11 -Wall -Wextra -Werror -Wno-main -fno-common -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections 
 COMPILE_CXX_FLAGS += -std=c++17 -Wall -Wextra -Werror -Wno-main -fno-common -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -fno-rtti -fno-exceptions 
 COMPILE_ASM_FLAGS += -c -x assembler-with-cpp
